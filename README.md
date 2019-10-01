fc5b00500d227f5d3b56902d7ef799a6b228e7dd
Boris Dalstein <dalboris@gmail.com>
2019-10-01 16:49:26 +0200

    Add .icns generation to iconify.sh; Refactor and clean repo; Update README

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # VGC Icons
 
-<img src="https://github.com/vgc/vgc-icons/blob/master/vgcillustration.png" width="128" height="128">
+<img src="https://github.com/vgc/vgc-icons/blob/master/vgcillustration/vgcillustration_256x256.png" width="128" height="128">
 
 ## About VGC
 
-VGC is a project initiated by VGC Software, whose goal is to implement professional graphic design and 2D animation tools based on the "Vector Graphics Complex". Developed by a collaboration of researchers from Inria Grenoble (France) and the University of British Columbia (Canada), this recent technology has the potential to bring radically more powerful yet intuitive tools to digital artists, such as coordinated editing of vector shapes sharing common boundaries, or automatic computation of inbetweens in 2D hand-drawn animation workflows.
+VGC is a project initiated by VGC Software, whose goal is to implement professional graphic design and 2D animation tools based on the "Vector Graphics Complex" technology. Developed by a collaboration of researchers from Inria Grenoble (France) and the University of British Columbia (Canada), this recent technology has the potential to bring radically more powerful yet intuitive tools to digital artists, such as coordinated editing of vector shapes sharing common boundaries, or automatic computation of inbetweens in 2D hand-drawn animation workflows.
 
 Putting it simply, it is a little bit of fresh ideas in a stagnating field!
 
@@ -12,23 +12,27 @@ For more information, please visit: https://www.vgc.io
 
 ## About this repository
 
-This repository hosts source SVG files and exported PNGs and JPGs for the VGC Icons.
+This repository hosts source SVG files as well as icons in various formats generated via the script `iconify.sh`.
 
-## Copyright and Permissions
+## Copyright, Licenses, and Permissions
 
-All SVG, PNG, and JPG files in this repository are Copyright (C) VGC Software. They are NOT licensed under any Creative Commons license.
+All files in this repository, including scripts and images, are Copyright (C) VGC Software.
+
+The script `iconify.sh` is licensed under the MIT license.
+
+The images in this repository are NOT licensed under any open source or Creative Commons license.
 
 The VGC Logo is a trademark of VGC Software.
 
-You are granted permission to use these images and the "VGC" name for creating and distributing Linux packages of VGC Illustration and VGC Animation, if built from unmodified versions of https://github.com/vgc/vgc.
+You are granted permission to use these images and the "VGC" name for creating and distributing Linux and FreeBSD packages of VGC Illustration and VGC Animation, if built from unmodified versions of https://github.com/vgc/vgc.
 
 You are granted permission to use these images and the "VGC" name in presentations and articles discussing about this project.
 
-You are NOT granted permission to use these images or the "VGC" Name for distributing any software library or application for Windows, macOS, Android, or iOS.
+You are NOT granted permission to use these images or the "VGC" Name for distributing any software library or application for Windows, macOS, Android, or iOS. In other words, on Windows, macOS, Android, or iOS, only official builds distributed by VGC Software are allowed to use these images or the "VGC" name.
 
 You are NOT granted permission to use these images in any software library or application using modified versions of https://github.com/vgc/vgc, either in source or binary form.
 
-You are NOT granted permission to use the "VGC" name in any software library or application using modified versions of https://github.com/vgc/vgc, either in source or binary form, except for the reproduction of copyright notices, as required for complying with the terms of the Apache 2.0 License.
+You are NOT granted permission to use the "VGC" name in any software library or application using modified versions of https://github.com/vgc/vgc, either in source or binary form, except for the reproduction of copyright notices, as required for complying with the terms of the Apache 2.0 License or MIT License.
 
 ## Contributing
 
--- iconify.sh
+++ iconify.sh
@@ -1,24 +1,71 @@
 #!/bin/bash
 #
-# Usage:
-#   iconify.sh myicon.svg
+# ------------------------------------------------------------------------------
+# Copyright (c) 2017-2019 VGC Software
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+#
+# This script should be run from macOS, which seems to be the only OS with the
+# capability of creating Apple's iconset format *.icns.
+#
+# In order to run this script, you need Inkscape, ImageMagick, and GIMP:
+#
+# brew cask install xquartz
+# brew cask install inkscape
+# brew install imagemagick
+# brew cask install gimp
+#
+# Then simply call:
+#
+# ./iconify.sh path/to/icon.svg
+#
+# Guidelines on icon design:
+#
+# https://developer.apple.com/design/human-interface-guidelines/macos/icons-and-images/app-icon/
 #
 
-# Specifies which image sizes we should generate. These must be ordered from
-# largest to smallest.
+# Which PNG image sizes should be generated, and which of
+# these should be automatically sharpened using GIMP.
 #
-png_sizes=(256)
-bmp_sizes=(48 32 24 16)
+png_sizes=(16 24 32 48 64 128 256 512 1024)
+png_sizes_sharpen=(16 24 32 48)
 
-# Useful variables
+# Which sizes to use for the generation of the ICO. They should be ordered
+# from largest to smallest.
 #
-in_svg=$1
-in_basename=${in_svg%.*}
-out_pngs=()
-out_bmps=()
-out_ico=$in_basename.ico
+ico_png_sizes=(256)
+ico_bmp_sizes=(48 32 24 16)
 
-# Create a PNG from a SVG using Inkscape.
+# Which sizes to use for the generation of the *.icns.
+#
+# Note: for the retina @2x versions, we simply use the PNG generated at twice
+# the resolution. For example, for 16x16@2x.png, we use the same image as for
+# 32x32.png. In theory, we would want these to be different, but in our case we
+# don't even simplify the design of smaller icons anyway, so it doesn't matter.
+# In the future, we may want to design simplified SVGs which would be used to
+# generate the smaller icons, or even directly design some of the PNGs or BMPs
+# without generating them from a SVG.
+#
+icns_sizes=(16 32 128 256 512)
+
+# Creates a PNG from a SVG using Inkscape.
 #
 make_png () {
     in_svg=$1
@@ -27,11 +74,17 @@ make_png () {
     inkscape -z -e "$out_png" -w $size -h $size "$in_svg" >/dev/null 2>/dev/null
 }
 
-# Applies sharpening using Gimp
+# Applies sharpening to a PNG using GIMP.
 #
+# Note that we have to call gimp directly without using a symlink. This is due
+# to a current bug/limitation of GIMP. See:
+# https://github.com/Homebrew/homebrew-cask/issues/69939
+# https://gitlab.gnome.org/Infrastructure/gimp-macos-build/issues/5
+#
+GIMP=$(readlink "$(which gimp)")
 sharpen_png () {
     png=$1
-    gimp -i -b "
+    $GIMP -i -b "
       (define
         (vgc-sharpen filename radius amount threshold)
         (let*
@@ -41,10 +94,10 @@ sharpen_png () {
           (gimp-file-save RUN-NONINTERACTIVE image drawable filename filename)
           (gimp-image-delete image)))
       (vgc-sharpen \"$png\" 5.0 0.30 0)
-      (gimp-quit 0)"
+      (gimp-quit 0)" >/dev/null 2>/dev/null
 }
 
-# Create a BMP from a PNG using ImageMagick.
+# Creates a BMP from a PNG using ImageMagick.
 #
 make_bmp () {
     in_png=$1
@@ -52,49 +105,74 @@ make_bmp () {
     convert "$in_png" -depth 8 -colors 256 "$out_bmp"
 }
 
-# Create all temporary PNGs.
+# Convert the input SVG file to an absolute path, which GIMP requires on macOS
+# as of GIMP 2.10.12.
+#
+in_svg="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")"
+in_basename=${in_svg%.*}
+echo "Input SVG file to iconify: $in_svg"
+
+# Create all PNG files.
 #
 for size in "${png_sizes[@]}"
 do
-    out_png=$in_basename.$size.png
-    out_pngs=( "${out_pngs[@]}" "$out_png" )
+    out_png=${in_basename}_${size}x${size}.png
     make_png "$in_svg" "$out_png" $size
+    echo "Generated $out_png"
 done
 
-# Create all temporary BMPs.
+# Sharpen the smaller PNG icon sizes.
 #
-for size in "${bmp_sizes[@]}"
+for size in "${png_sizes_sharpen[@]}"
 do
-    out_png=$in_basename.$size.png
-    out_bmp=$in_basename.$size.bmp
-    out_bmps=( "${out_bmps[@]}" "$out_bmp" )
-    make_png "$in_svg" "$out_png" $size
+    out_png=${in_basename}_${size}x${size}.png
     sharpen_png "$out_png"
-    make_bmp "$out_png" "$out_bmp"
+    echo "Sharpened $out_png"
 done
 
-# Compile all the generated PNGs and BMPs into one .ico file. Note how we only
-# apply -colors 256 to the smaller sizes, to prevent degrading the high-res PNG
-# icon.
+# Create the ICO by converting PNGs to BMPs, calling ImageMagick,
+# then deleting the BMPs.
+#
+# Note how we only apply -colors 256 to the smaller sizes, to prevent
+# degrading the high-res PNG icon.
 #
 # Also note that we order them from higher-res to lower-res in the *.ico, which
 # leads to better previews in some systems, and seems preferable.
 #
-convert "${out_pngs[@]}" "${out_bmps[@]}" "$out_ico"
-
-# Remove all temporary PNGs.
-#
-for size in "${png_sizes[@]}"
+ico_pngs=()
+for size in "${ico_png_sizes[@]}"
 do
-    out_png=$in_basename.$size.png
-    rm "$out_png"
+    ico_png=${in_basename}_${size}x${size}.png
+    ico_pngs=( "${ico_pngs[@]}" "$ico_png" )
 done
-
-# Remove all temporary BMPs.
-#
-for size in "${bmp_sizes[@]}"
+ico_bmps=()
+for size in "${ico_bmp_sizes[@]}"
 do
-    out_png=$in_basename.$size.png
-    out_bmp=$in_basename.$size.bmp
-    rm "$out_png" "$out_bmp"
+    ico_png=${in_basename}_${size}x${size}.png
+    ico_bmp=${in_basename}_${size}x${size}.bmp
+    ico_bmps=( "${ico_bmps[@]}" "$ico_bmp" )
+    make_bmp "$ico_png" "$ico_bmp"
 done
+out_ico=${in_basename}.ico
+convert "${ico_pngs[@]}" "${ico_bmps[@]}" "$out_ico"
+rm "${ico_bmps[@]}"
+echo "Generated $out_ico"
+
+# Create the ICNS by copying PNGs to a temporary folder, calling iconutil,
+# then deleting the folder.
+#
+icns_dir=${in_basename}.iconset
+mkdir "$icns_dir"
+for size in "${icns_sizes[@]}"
+do
+    in_png=${in_basename}_${size}x${size}.png
+    out_png=${icns_dir}/icon_${size}x${size}.png
+    size_2x=$(( 2 * size ))
+    in_png_2x=${in_basename}_${size_2x}x${size_2x}.png
+    out_png_2x=${icns_dir}/icon_${size}x${size}@2x.png
+    cp "$in_png" "$out_png"
+    cp "$in_png_2x" "$out_png_2x"
+done
+iconutil -c icns "$icns_dir"
+rm -r "$icns_dir"
+echo "Generated ${in_basename}.icns"
